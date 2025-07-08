fb4612044220a11d7a6b99f2bfb2f47bfd32f20d
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-07-07 21:01:08 -0300

    feat: install fastify and create api.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("hello world");
+import fastify from "fastify"
+
+const server = fastify({logger: false})
+
+server.get("/teams", async (request, reply) => {
+  reply.type("application/json").code(200)
+  return [{ id: 1, name: "maclaren" }]
+})
+
+server.listen({ port: 3333 }, () => {
+  console.log("Server is runnig")
+})
